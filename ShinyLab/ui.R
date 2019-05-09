01edd3df829957edc54404537ff1aea8334c4f08
Krishna Pramod Kumar <kpramodkumar@ucdavis.edu>
2019-05-09 21:41:06 +0000

    hey ria

--- ShinyLab/ui.R
+++ ShinyLab/ui.R
@@ -10,24 +10,28 @@
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(fluidPage( #create the overall page
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("RICE Data"),
   
-  # Sidebar with a slider input for number of bins 
+  # Some helpful information
+  helpText("This application creates a boxplot to show difference between",
+           "iris species.  Please use the radio box below to choose a trait",
+           "for plotting"),
+  
+  # Sidebar with a radio box to input which trait will be plotted
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
+      radioButtons("species", #the input variable that the value will go into
+                   "Choose a species to display:",
+                   c("setosa",
+                     "versicolor",
+                     "virginica")
+      )),
     
     # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+    mainPanel(plotOutput("boxPlot")
     )
   )
 ))
