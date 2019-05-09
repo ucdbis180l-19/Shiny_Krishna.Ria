6552b8dd2ecc945a4e56a39afc55ebfd7d296db8
Ubuntu <ubuntu@ip-172-31-42-186.ec2.internal>
2019-05-09 21:57:15 +0000

    alterations to server

--- ShinyLab/server.R
+++ ShinyLab/server.R
@@ -7,22 +7,25 @@
 #    http://shiny.rstudio.com/
 #
 
+# Plant height, Seed length, Seed width, Seed volume, Seed surface area, Protein content, Amylose content
+
+
 
 library(shiny)
 library(ggplot2)
+library(tidyverse)
 
 # Define server logic required to draw a boxplot
 shinyServer(function(input, output) {
   
-  # Expression that generates a boxplot. The expression is
+  # Expression that generates a scatterplot. The expression is
   # wrapped in a call to renderPlot to indicate that:
   #
   #  1) It is "reactive" and therefore should re-execute automatically
   #     when inputs change
   #  2) Its output type is a plot
-  species_iris <- iris$Species
 
-  output$boxPlot <- renderPlot({
+  output$scatterPlot <- renderPlot({
     
     # set up the plot
     pl <- ggplot(data = iris,
