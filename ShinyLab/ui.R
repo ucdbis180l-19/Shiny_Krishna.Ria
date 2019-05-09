f8e934666a36e3dcc49010e6cd8aaa0970bc7c4e
Krishna Pramod Kumar <kpramodkumar@ucdavis.edu>
2019-05-09 22:19:46 +0000

    ui commit

--- ShinyLab/ui.R
+++ ShinyLab/ui.R
@@ -24,19 +24,37 @@ shinyUI(fluidPage( #create the overall page
   # Sidebar with a radio box to input which trait will be plotted
   sidebarLayout(
     sidebarPanel(
-      radioButtons("traits", #the input variable that the value will go into
-                   "Choose a trait to display:",
+      radioButtons("traits1", #the input variable that the value will go into
+                   "Choose your first trait to display:",
                    c("Plant height",
                      "Seed length",
                      "Seed width",
                      "Seed volume",
                      "Seed surface area",
                      "Protein content",
-                     "Amylose content")
-      )),
+                     "Amylose content"),
+      radioButtons("traits2", #the input variable that the value will go into
+                  "Choose your second trait to display:",
+                  c("Plant height",
+                    "Seed length",
+                    "Seed width",
+                    "Seed volume",
+                    "Seed surface area",
+                    "Protein content",
+                    "Amylose content"),
+      radioButtons("traits3", #the input variable that the value will go into
+                   "Choose your second trait to display:",
+                   c("Plant height",
+                   "Seed length",
+                   "Seed width",
+                   "Seed volume",
+                   "Seed surface area",
+                   "Protein content",
+                   "Amylose content")
+      ))),
     
     # Show a plot of the generated distribution
     mainPanel(plotOutput("scatterPlot")
     )
   )
-))
+)))
