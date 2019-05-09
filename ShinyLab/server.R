0f141187afb8f8a9f437a70310c6276ca0220e65
Krishna Pramod Kumar <kpramodkumar@ucdavis.edu>
2019-05-09 21:49:33 +0000

    changes

--- ShinyLab/server.R
+++ ShinyLab/server.R
@@ -8,19 +8,32 @@
 #
 
 library(shiny)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
+
+# Define server logic required to draw a boxplot
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
   
+  # Expression that generates a boxplot. The expression is
+  # wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should re-execute automatically
+  #     when inputs change
+  #  2) Its output type is a plot
+  
+  output$boxPlot <- renderPlot({
+    
+    # set up the plot
+    pl <- ggplot(data = iris,
+                 #Use aes_string below so that input$trait is interpreted
+                 #correctly.  The other variables need to be quoted
+                 aes_string(x=c("Sepal.Length"),
+                            y=input$species,
+                            fill="Species"
+                 )
+    )
+    
+    # draw the boxplot for the specified trait
+    pl + geom_boxplot()
+  })
 })
